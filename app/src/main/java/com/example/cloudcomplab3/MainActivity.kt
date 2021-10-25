606876d5027187fef37f2b1a9cde23f1a65bb43e
phoenixredwolf <b.vogtman@outlook.com>
2021-10-25 14:44:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cloudcomplab3/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.cloudcomplab3
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cloudcomplab3.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        // prepare our List view and RecyclerView (cells)
+        setupRecyclerView(item_list)
+    }
+
+    // recycler view is the list of cells
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+
+        // update individual cell when the Note data are modified
+        UserData.notes().observe(this, Observer<MutableList<UserData.Note>> { notes ->
+            Log.d(TAG, "Note observer received ${notes.size} notes")
+
+            // let's create a RecyclerViewAdapter that manages the individual cells
+            recyclerView.adapter = NoteRecyclerViewAdapter(notes)
+        })
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
