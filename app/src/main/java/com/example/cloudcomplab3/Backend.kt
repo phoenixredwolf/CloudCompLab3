60fc96bd0b87ef859684a264f56dc9af5d6d210b
phoenixredwolf <b.vogtman@outlook.com>
2021-11-02 13:46:25 -0400

    added classes

---
+++ app/src/main/java/com/example/cloudcomplab3/Backend.kt
@@ -0,0 +1,22 @@
+package com.example.cloudcomplab3
+
+import android.content.Context
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
+
+object Backend {
+
+    private const val TAG = "Backend"
+
+    fun initialize(applicationContext: Context) : Backend {
+        try {
+            Amplify.configure(applicationContext)
+            Log.i(TAG, "Initialized Amplify")
+        } catch (e: AmplifyException) {
+            Log.e(TAG, "Could not initialize Amplify", e)
+        }
+        return this
+    }
+
+}
